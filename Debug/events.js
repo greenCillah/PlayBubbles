0c944e084cc25a088d7b7d99694ad9e61a6df1e6
GreenCillah <thecoolgreenman@gmail.com>
2018-05-02 22:30:32 +1000

    debug

---
+++ Debug/events.js
@@ -0,0 +1,27 @@
+function events() {
+
+    this.eventList = [];
+    this.x = 1200;
+    this.y = 400;
+    this.gap = 18;
+
+    this.show = function () {
+        textSize(20);
+        fill(0, 102, 153);
+
+        // console.log(this.eventList.length);
+
+        var j = 0;
+        for (var i = this.eventList.length - 1; i >= 0; i--) {
+            text(i + "[  ]" + this.eventList[i], this.x, this.y + (this.gap * j));
+            j++;
+        }
+
+    }
+
+    this.addEvent = function (newEvent) {
+        this.eventList.push(newEvent);
+    }
+
+
+}
--- game.js
+++ game.js
@@ -15,6 +15,7 @@ var boundaries = [];
 var texts = [];
 var width = 30;
 var scoreBoard;
+var debugEvents;
 
 var ground;
 
@@ -26,17 +27,18 @@ function setup() {
     //Engine.run(engine);
 
     scoreBoard = new scoreBoard(0, 0);
+    debugEvents = new events();
 
     //top left  diagonal
-    boundaries.push(new Boundary(100, 100, width * 0.6, 20, 0.3));
+    boundaries.push(new Boundary(100, 100, 800, 20, 0.3));
     //top right diagonal
-    boundaries.push(new Boundary(500, 300, width * 0.6, 20, -0.3));
+    boundaries.push(new Boundary(500, 300, 800, 20, -0.3));
     //middle left diagonal
-    boundaries.push(new Boundary(100, 500, width * 0.6, 20, 0.3));
+    boundaries.push(new Boundary(100, 500, 800, 20, 0.3));
     //middle right diagonal
-    boundaries.push(new Boundary(500, 700, width * 0.6, 20, -0.3));
+    boundaries.push(new Boundary(500, 700, 800, 20, -0.3));
     //bottom left diagonal
-    boundaries.push(new Boundary(100, 900, width * 0.6, 20, 0.3));
+    boundaries.push(new Boundary(100, 900, 800, 20, 0.3));
 
     // bottom
     boundaries.push(new Boundary(400, 1100, width, 20, 0));
@@ -66,8 +68,7 @@ function mousePressed() {
 
     }
 
-    texts.push(new Text(mouseX, mouseY, mouseX + ":" + mouseY));
-
+    debugEvents.addEvent(mouseX + ":" + mouseY);
 
 }
 
@@ -75,14 +76,14 @@ function draw() {
     background(51);
     Engine.update(engine);
 
-    var j, k;
-
-    for (j = 0; j < circles.length; j++) {
+    for (var j = 0; j < circles.length; j++) {
         circles[j].show();
     }
+
     for (var i = 0; i < boxes.length; i++) {
         boxes[i].show();
     }
+
     for (var i = 0; i < boundaries.length; i++) {
         boundaries[i].show();
     }
@@ -92,5 +93,6 @@ function draw() {
     }
 
     scoreBoard.show();
+    debugEvents.show();
 
 }
