cddbf318db6546770f2fe071c2b9d4e99170f4ba
GreenCillah <thecoolgreenman@gmail.com>
2018-05-22 20:44:10 +1000

    holding pen

---
+++ holdingPen/arrayPlay.js
@@ -0,0 +1,29 @@
+var arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k'];
+
+
+var length = arr.length;
+
+for (var i = 0; i < length; i++) {
+
+
+    if (arr[i] === 'd') {
+        arr.splice(i, 1);
+
+    }
+
+    if (arr[i] === undefined) {
+        continue;
+    }
+
+    document.body.innerHTML = document.body.innerHTML + i + " - " + arr[i] + " <br>";
+
+}
+
+
+document.body.innerHTML += "---------------------<br>";
+
+arr.forEach(function (element, index) {
+
+    document.body.innerHTML = document.body.innerHTML + index + " = " + element + " " + " <br>";
+
+});
